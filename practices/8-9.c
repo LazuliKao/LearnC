dd2e194c4e15ef3c0ed5682f45a0290b096382e9
LazuliKao <lazulikao@nbt.edu.cn>
2023-03-20 18:27:32 +0800

    sync

---
+++ practices/8-9.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAXS 15
+
+void StringCount(char *s);
+void ReadString(char *s); /* 由裁判实现，略去不表 */
+
+int main()
+{
+    char s[MAXS];
+
+    ReadString(s);
+    StringCount(s);
+
+    return 0;
+}
+
+/* Your function will be put here */
+void StringCount(char *s)
+{
+    int upper = 0, lower = 0, space = 0, digit = 0, other = 0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            upper++;
+        else if (s[i] >= 'a' && s[i] <= 'z')
+            lower++;
+        else if (s[i] == ' ')
+            space++;
+        else if (s[i] >= '0' && s[i] <= '9')
+            digit++;
+        else
+            other++;
+    }
+    printf("%d %d %d %d %d", upper, lower, space, digit, other);
+}
