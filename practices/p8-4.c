6b102e7e8d5262448003903f28d13665fe77b751
LazuliKao <lazulikao@nbt.edu.cn>
2023-03-20 17:51:02 +0800

    Update p8-4.c

--- practices/p8-4.c
+++ practices/p8-4.c
@@ -54,21 +54,14 @@ output:
 // }
 void CountOff(int n, int m, int out[])
 {
-    int temp[MAXN]; // 队伍
+    int temp[MAXN]; // 是否已经出列
     for (int i = 0; i < n; i++)
-    {
-        temp[i] = i;
-    }
-    int current = 0;
-    for (int i = 0; i < n; i++)
-    {
-        out[i] = 1;
-        // int remain = n - i;
-        // current = (current + m) % remain;
-        // out[current - 1] = temp[current - 1];
-        // for (int j = current - 1; j < remain; j++)
-        // {
-        //     temp[current] = temp[current + 1];
-        // }
-    }
+        temp[i] = 1; // 默认存在
+    int current = 0, count = 0;
+    for (int i = 0; current < n; i = (i + 1) % n)
+        if (temp[i] && ++count == m)
+        {
+            count = temp[i] = 0;
+            out[i] = ++current;
+        }
 }
