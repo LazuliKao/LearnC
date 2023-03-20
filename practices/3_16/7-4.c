747166ccfb832d892baec07b3effe27281dc810d
LazuliKao <lazulikao@nbt.edu.cn>
2023-03-20 13:29:26 +0800

    sync

--- practices/3_16/7-4.c
+++ practices/3_16/7-4.c
@@ -1,8 +1,31 @@
+// #include "stdio.h"
+// int main()
+// {
+//     int count;
+//     scanf("%d", &count);
+//     float max = 0;
+//     float min = 100;
+//     float sum = 0;
+//     for (int i = 0; i < count; i++)
+//     {
+//         float current = 0;
+//         scanf("%f", &current);
+//         if (current > max)
+//             max = current;
+//         if (current < min)
+//             min = current;
+//         sum += current;
+//     }
+//     printf("%.1f %.1f %.1f", max, min, sum / count);
+//     return 0;
+// }
 #include "stdio.h"
 int main()
 {
     int count;
     scanf("%d", &count);
+    double s[10];
+
     float max = 0;
     float min = 100;
     float sum = 0;
--- practices/3_16/7-5.c
+++ practices/3_16/7-5.c
@@ -5,24 +5,23 @@ int main()
     double a, b, c;
     scanf("%lf%lf%lf", &a, &b, &c);
     double delta = b * b - 4 * a * c;
-    if (a == 0 && b == 0 && c == 0)
+    if (a == 0 && b == 0 && c == 0) // find1
     {
         printf("Any Answer");
     }
-    else if (a == 0 && b == 0 && c != 0)
+    else if (a == 0 && b == 0 && c != 0) // find2
     {
         printf("No Answer");
     }
-    else if (a == 0)
+    else if (a == 0) // find10
     {
-        double result = -b / c;
-        printf("x=%.2lf", result);
+        printf("x=%.2lf", -c / b);
     }
-    else if (delta > 0)
+    else if (delta > 0) // find5
     {
-        double x1 = (-b + sqrt(delta)) / (2 * a);
-        double x2 = (-b - sqrt(delta)) / (2 * a);
-        if (x1 > x2)
+        double x1 = (-b + sqrt(delta)) / (2. * a);
+        double x2 = (-b - sqrt(delta)) / (2. * a);
+        if (x1 < x2)
         {
             double tmp = x1;
             x1 = x2;
@@ -30,15 +29,15 @@ int main()
         }
         printf("x1=%.2lf,x2=%.2lf", x1, x2);
     }
-    else if (delta < 0)
+    else if (delta < 0) // find3
     {
-        double p1 = (-b) / 2 * a;
-        double p2 = fabs(sqrt(-delta) / (2 * a));
+        double p1 = (-b) / (2. * a);
+        double p2 = fabs(sqrt(-delta) / (2. * a));
         printf("x1=%.2lf+%.2lfi,x2=%.2lf-%.2lfi", p1, p2, p1, p2);
     }
-    else
+    else // find4
     {
-        double x = (-b) / (2 * a);
+        double x = (-b) / (2. * a);
         printf("x1=x2=%.2lf", x);
     }
     return 0;
