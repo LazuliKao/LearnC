2beb3caaefdacc4971148b3cfe57fd1fd3ce56e6
LazuliKao <lazulikao@nbt.edu.cn>
2023-03-23 17:52:16 +0800

    Update 6-6.c

--- practices/6-6.c
+++ practices/6-6.c
@@ -13,11 +13,12 @@ int main()
 }
 
 /* 你的代码将被嵌在这里 */
-int reverse(int number){
+int reverse(int number)
+{
     int result = 0;
-    while (number != 0){
+    do
+    {
         result = result * 10 + number % 10;
-        number /= 10;
-    }
+    } while (number /= 10);
     return result;
 }
